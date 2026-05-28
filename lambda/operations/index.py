ff4e2fb71c4cd32233ed4068eb3862f642bb9759
liamade <liamcallahan1023@gmail.com>
2026-05-28 18:17:16 -0400

    adding simple test slash command to make sure pipeline works

--- lambda/operations/index.py
+++ lambda/operations/index.py
@@ -14,3 +14,19 @@ def handler(event, context):
                 "type": 1  # respond with type 1 to acknowledge the ping request
             })
         }
+    if body["type"] == 2:
+
+        command = body["data"]["name"]  # capture the slash command
+        subcommand = body["data"]["options"][0]["name"]  # capture the slash command's subcommand - words are fun
+
+        if command == "server":
+            if subcommand == "start":
+                return {
+                    "statusCode": 200,  # respond with a 200 status code to tell Discord the command was received and is being processed
+                    "body": json.dumps({
+                        "type": 4,  # respond immediately to the user
+                        "data": {
+                            "content": "It actually works! God bless"  # message sent back to the user
+                        }
+                    })
+                }
